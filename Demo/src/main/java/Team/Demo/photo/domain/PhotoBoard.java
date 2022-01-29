6d2cd19c48579f7751338923e475cfcbaf513e35
reosong <ambition_1018@naver.com>
2022-01-29 13:19:31 +0900

    start

---
+++ Demo/src/main/java/Team/Demo/photo/domain/PhotoBoard.java
@@ -0,0 +1,17 @@
+package team.demo.photo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhotoBoard {
+
+    private String member;
+    private int number;
+    private String date;
+    private String photo1;
+    private String photo2;
+    private 
+
+}
