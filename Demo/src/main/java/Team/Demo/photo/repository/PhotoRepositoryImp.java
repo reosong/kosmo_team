bf39fb6e17da961a0ac20a724547ec2c2790a660
reosong <ambition_1018@naver.com>
2022-02-01 16:32:46 +0900

    set

--- Demo/src/main/java/Team/Demo/photo/repository/PhotoRepositoryImp.java
+++ Demo/src/main/java/Team/Demo/photo/repository/PhotoRepositoryImp.java
@@ -1,7 +1,13 @@
 package team.demo.photo.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class PhotoRepositoryImp implements PhotoRepository{
 
+    public void showBoard() {
+        
+    }
 
 
 }
