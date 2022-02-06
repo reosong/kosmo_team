be226428485c9fe75bb352c26f4fbefb6c44b663
reosong <ambition_1018@naver.com>
2022-02-06 15:09:51 +0900

    first commit

---
+++ src/main/java/Team/demo/ServletInitializer.java
@@ -0,0 +1,14 @@
+package Team.demo;
+
+import Team.DemoApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+}
---
+++ src/test/java/Team/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Team.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
